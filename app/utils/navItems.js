58983ecbe7744e74719b4cd459b86e633535971a
Jay Winebrenner <jaywinebrenner@gmail.com>
2024-04-24 10:44:13 -0700

    wire up basic scaffolding

---
+++ app/utils/navItems.js
@@ -0,0 +1,39 @@
+
+
+export const NavItems = [
+    {
+        title: "Painting",
+        imageSrc: "/2.jpg",
+        url: "/painting"
+    },
+    {
+        title: "Illustration",
+        imageSrc: "/3.jpg",
+        url: "/illustration"
+    },
+    {
+        title: "Animation",
+        imageSrc: "/4.jpg",
+        url: "/animation"
+    },
+    {
+        title: "Melted Radio",
+        imageSrc: "/5.jpg",
+        url: "/melted-radio"
+    },
+    {
+        title: "Free Spirit News",
+        imageSrc: "/6.jpg",
+        url: "painting"
+    },
+    {
+        title: "CV",
+        imageSrc: "/7.jpg",
+        url: "/cv"
+    },
+    {
+        title: "Shop",
+        imageSrc: "/8.jpg",
+        url: "/shop"
+    },
+]
