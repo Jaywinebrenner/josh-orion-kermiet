61b2a944f7b7c5ec8ca4f1c4b3aafc67c53c7e58
Jay Winebrenner <jaywinebrenner@gmail.com>
2024-04-25 10:05:06 -0700

    use memo to stop blinking

--- app/animation/page.js
+++ app/animation/page.js
@@ -1,38 +1,5 @@
 
-// import { paintings } from '../utils/paintings'
-// import Navigation from "../components/Navigation";
-// import { animations } from '../utils/animations';
-// import React from 'react'
-
-// const Animation = () => {
-
-//   console.log(animations)
-//   return (
-//     <div className='animations'>
-//       <div className='container'>
-//         <Navigation/>
-//         <div className='animations-wrapper'>
-//           {
-//               animations && animations.map((animation, i) => {
-//                 return (
-//                   <div key={`animation-key=${i}`} className='animation-wrapper'>
-//                     <img src={animation.imageSrc}/>
-//                     <p>{animation.title }</p>
-//                   </div>
-//                 )
-//               })
-//           }
-//         </div>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default Animation
-
-
 "use client"
-import { paintings } from '../utils/paintings';
 import Navigation from '../components/Navigation';
 import { animations } from '../utils/animations';
 import React, { useEffect, useState } from 'react';
--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -1,44 +1,41 @@
 "use client"
 import React from 'react';
-import { usePathname } from 'next/navigation'
+import { usePathname } from 'next/navigation';
 import { NavItems } from '../utils/navItems';
 import Link from 'next/link';
 
 const Navigation = () => {
-
-
-  const pathname = usePathname()
+  const pathname = usePathname();
 
   const renderLink = (link, i) => (
-  
-   
     <a
       key={`breadcrumb-key=${i}`}
       className={pathname === link.url ? 'breadcrumb active' : 'breadcrumb'}
       href={link.url}
       target={link.newPage ? '_blank' : '_self'}
-      rel={link.newPage ? 'noreferrer' : ''} 
+      rel={link.newPage ? 'noreferrer' : ''}
     >
       {link.title}
     </a>
   );
-  
 
   return (
     <div className="navigation">
       <div className="logo-wrapper">
         <img src="/logo.png" alt="Logo" />
       </div>
-      {pathname !== "/" && <div className="breadcrumbs">
-        <Link href="/" passHref legacyBehavior>
-          <a className={pathname === '/' ? 'breadcrumb active' : 'breadcrumb'}>
-            Home
-          </a>
-        </Link>
-        {NavItems && NavItems.map((link, i) => renderLink(link, i))}
-      </div>}
+      {pathname !== '/' && (
+        <div className="breadcrumbs">
+          <Link href="/" passHref legacyBehavior>
+            <a className={pathname === '/' ? 'breadcrumb active' : 'breadcrumb'}>
+              Home
+            </a>
+          </Link>
+          {NavItems && NavItems.map((link, i) => renderLink(link, i))}
+        </div>
+      )}
     </div>
   );
 };
 
-export default Navigation;
+export default React.memo(Navigation);
